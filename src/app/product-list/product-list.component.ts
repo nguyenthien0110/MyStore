94af9f40c193f647541a49323d22f35830d8bfa8
ThienNT <Nguyenthien11082005@gmail.com>
2024-06-07 00:06:32 +0700

    fix select all field product

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { ProductService } from '../service/product.service';
 import { CartService } from '../service/cart.service';
@@ -9,33 +8,41 @@ import { Product } from '../model/product.model';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css',
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
   products: Product[] = [];
-  @Output() addToCartEvent = new EventEmitter<any>();
-  selectedQuantity: number = 1;
+  selectedQuantities: { [productId: number]: number } = {};
 
   constructor(
     private productService: ProductService,
     private router: Router,
     private cartService: CartService,
     private _snackBar: MatSnackBar
-  ) {
+) {
     this.products = productService.getAllProducts();
-  }
-  ngOnInit(): void {}
+    this.products.forEach((product) => {
+        if (!this.selectedQuantities.hasOwnProperty(product.id)) {
+            this.selectedQuantities[product.id] = 1;
+        }
+    });
+}
 
   navigateToProductDetail(productId: number): void {
     this.router.navigate(['/product', productId]);
   }
 
-  addToCart(product: Product, selectedQuantity: number): void {
+  addToCart(product: Product): void {
+    const selectedQuantity = this.selectedQuantities[product.id];
     this.cartService.addToCart(product, selectedQuantity);
     this._snackBar.open('Added data successfully', 'Close', {
       duration: 3000,
       verticalPosition: 'top',
       horizontalPosition: 'right',
     });
+  }
+
+  updateSelectedQuantity(product: Product, quantity: number): void {
+    this.selectedQuantities[product.id] = quantity;
   }
 }
